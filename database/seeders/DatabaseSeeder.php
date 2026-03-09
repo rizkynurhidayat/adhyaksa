9b9ac03c88fff7c83459f58559c107f001165c43
M Hilmi Anis Pramana <bglhil06@gmail.com>
2026-03-09 09:51:20 +0700

    seeder dan data dummy (demo)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,4 +23,13 @@ class DatabaseSeeder extends Seeder
             ]
         );
     }
+
+    public function run(): void
+    {
+        // Panggil seeder yang baru saja kita buat
+        $this->call([
+            ProductSeeder::class,
+            // UserSeeder::class, (kalau nanti ada seeder lain, tambahkan di sini)
+        ]);
+    }
 }
